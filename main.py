d20ce618b20a80028609c5d6eff272a0c6d4920c
rkeenan <ross.keenan@protonmail.comm>
2023-07-05 20:43:41 -0500

    skipping first data row, because header is two lines instead of one.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tabula
+import pandas as pd
 
 tables = tabula.read_pdf("slstax_rates_7-1-2023.pdf",
                          pages="all")
-df = tables[0].to_string(header=False)
+
+df = tables[0]
+df = df.iloc[1:].to_string(header=False)
 print(df)
