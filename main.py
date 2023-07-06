af9f7dd78c6047c71230b2a6b4b6c938cf34ad09
rkeenan <ross.keenan@protonmail.comm>
2023-07-05 20:55:40 -0500

    looping through tables and grabbing the data that we need.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tabula
 import pandas as pd
 
 tables = tabula.read_pdf("slstax_rates_7-1-2023.pdf",
-                         pages="all")
+                         pages="all", multiple_tables=True)
 
-df = tables[0]
-df = df.iloc[1:].to_string(header=False)
-print(df)
+for df in tables:
+    df = df.iloc[1:].to_string(header=False)
+    print(df)
