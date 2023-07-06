0745a4ecfd3da1ac82756e88facc672f373c07b5
rkeenan <ross.keenan@protonmail.comm>
2023-07-05 21:56:12 -0500

    updated variable name, so it was not being switched from a string to a buffer.

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import urllib.request
 import os
 
 url = 'https://revenue.nebraska.gov/sites/revenue.nebraska.gov/files/doc/slstax_rates_7-1-2023.pdf'
-write_file = 'Nebraska tax rates.pdf'
+write_file_name = 'Nebraska tax rates.pdf'
 
-with urllib.request.urlopen(url) as response, open(write_file, 'wb') as write_file:
+with urllib.request.urlopen(url) as response, open(write_file_name, 'wb') as write_file:
     data = response.read()
     write_file.write(data)
 
-tables = tabula.read_pdf('Nebraska tax rates.pdf',
+tables = tabula.read_pdf(write_file_name,
                          pages="all", multiple_tables=True)
 
 for df in tables:
     df = df.iloc[1:].to_string(header=False)
+    # Todo: add datedPulled field
+    # Todo: add effectiveDate from heading on PDF using Regex
     print(df)
 
-os.remove('Nebraska tax rates.pdf')
+os.remove(write_file_name)
